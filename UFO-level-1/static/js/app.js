64f1bace69b34fd0ec1debe9a562004b369e8ce1
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-11 13:20:35 -0500

    console.log each sighting

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,12 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// get a reference to the table body
+var tbody = d3.select("tbody");
+
+console.log(tableData);
+
+// loop thru table and console log each row
+tableData.forEach(function(sighting) {
+    console.log(sighting);
+});
