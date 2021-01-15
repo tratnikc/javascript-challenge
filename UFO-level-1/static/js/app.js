13084e1edb0addeffa44dec92e32a24635a42a35
tratnikc <68667850+tratnikc@users.noreply.github.com>
2021-01-15 16:38:21 -0500

    clean up

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,35 +1,8 @@
 // from data.js
 var tableData = data;
 
-// get a reference to the table body
-var tbody = d3.select("tbody");
+showFilteredData(tableData);
 
-console.log(tableData);
-
-// step 1 - append table to webpage
-// loop thru table
-// add table row 'tr' for each sighting
-// use Object.entries to append column data
-tableData.forEach(function(sighting) {
-    // console.log(sighting);
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(function([key,value]) {
-        // append a cell to the row for each value
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
-
-// // refactor to use arrow functions
-// tableData.forEach((sighting) => {
-//     var row = tbody.append("tr");
-//     Object.entries(sighting).forEach(([key,value]) => {
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
-
-// step 2 - filter data using date
 // select button using ID
 var filterButton = d3.select("#filter-btn");
 
@@ -51,18 +24,17 @@ function runEnter() {
 
     var filteredData = tableData.filter(sighting => new Date(sighting.datetime).toDateString() === new Date(inputDateValue).toDateString());
 
-    console.log(filteredData);
-
     if (inputDateValue == "") {
         showFilteredData(tableData);
     }
     else {
         showFilteredData(filteredData);
     }
+
 };
 
 function showFilteredData(filtered) {
-    var sightings = filtered;
+    // get a reference to the table body
     var tbody = d3.select("tbody");
     // clear webpage table
     tbody.html("");
@@ -74,5 +46,5 @@ function showFilteredData(filtered) {
             cell.text(value);
         });
     });
-    console.log(filtered.length);
-}
+    console.log(filtered);
+};
